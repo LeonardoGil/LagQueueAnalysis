432df70f88085161a6d08806b3f0aaa962670291
Leonardo Albuquerque Gil <leonardo.gil@nddcombr.onmicrosoft.com>
2024-08-05 20:32:55 -0300

    Desenvolvendo camada de Dominio

---
+++ LagQueueDomain/Entities/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LagQueueDomain.Entities
+{
+    public class Message
+    {
+        public string Type { get; set; }
+
+        public int Position { get; set; }
+
+        public Guid MessageId { get; set; }
+
+        public string Payload { get; set; }
+
+        public uint Expiration { get; set; }
+
+        
+        public Guid QueueId { get; set; }
+
+        public virtual Queue Queue { get; set; }
+        
+        
+        public Guid ReplyToId { get; set; }
+
+        public virtual Queue ReplyTo { get; set; }
+    }
+}
---
+++ LagQueueDomain/Settings/RabbitMQSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace LagQueueDomain.Settings
+{
+    public class RabbitMQSettings
+    {
+        public string Url { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Token()
+        {
+            var pass = $"{Username}:{Password}";
+
+            var token = Convert.ToBase64String(Encoding.ASCII.GetBytes(pass));
+
+            return $"Basic {token}";
+        }
+    }
+}
