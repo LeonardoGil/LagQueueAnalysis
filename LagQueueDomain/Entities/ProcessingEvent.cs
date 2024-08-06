0fe90edaf7f0346d55e066c7432dec42ff1b6278
Leonardo Albuquerque Gil <leonardo.gil@nddcombr.onmicrosoft.com>
2024-08-06 08:19:17 -0300

    Adicionado entidade ProcessingEvent

---
+++ LagQueueDomain/Entities/ProcessingEvent.cs
@@ -0,0 +1,44 @@
+﻿using LagQueueDomain.Enums;
+using System;
+
+namespace LagQueueDomain.Entities
+{
+    public class ProcessingEvent
+    {
+        public ProcessingEvent() { }
+
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public DateTime Start { get; private set; }
+
+        public DateTime End { get; private set; }
+
+        public ProcessingEventStatusEnum Status { get; private set; }
+
+        public string Exception { get; private set; }
+
+        public void SetSucess()
+        {
+            Status = ProcessingEventStatusEnum.Sucess;
+            End = DateTime.UtcNow;
+        }
+
+        public void SetFail(Exception exception)
+        {
+            Status = ProcessingEventStatusEnum.Fail;
+            Exception = exception.Message;
+            End = DateTime.UtcNow;
+        }
+
+        public static ProcessingEvent Create(string name)
+        {
+            return new ProcessingEvent()
+            {
+                Id = Guid.NewGuid(),
+                Name = name
+            };
+        }
+    }
+}
