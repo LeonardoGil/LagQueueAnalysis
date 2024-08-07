9f2b6230a1313a5c1cb591e1f576c1ce0c0f4c81
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-08-07 00:09:21 -0300

    Configurando EntityFramework e Migration Inicial

---
+++ LagQueueApplication/EFContexts/LagQueueContext.cs
@@ -0,0 +1,22 @@
+﻿using LagQueueDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LagQueueApplication.EFContexts
+{
+    public class LagQueueContext : DbContext
+    {
+        public LagQueueContext(DbContextOptions<LagQueueContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Queue> Queues { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+        
+        public DbSet<ProcessingEvent> ProcessingEvents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ LagQueueApplication/Migrations/20240807030616_Init.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LagQueueApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcessingEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Exception = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessingEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Queues",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Queues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Position = table.Column<int>(type: "int", nullable: false),
+                    Payload = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Expiration = table.Column<long>(type: "bigint", nullable: false),
+                    QueueId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReplyToId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_Messages_Queues_QueueId",
+                        column: x => x.QueueId,
+                        principalTable: "Queues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Queues_ReplyToId",
+                        column: x => x.ReplyToId,
+                        principalTable: "Queues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_QueueId",
+                table: "Messages",
+                column: "QueueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReplyToId",
+                table: "Messages",
+                column: "ReplyToId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "ProcessingEvents");
+
+            migrationBuilder.DropTable(
+                name: "Queues");
+        }
+    }
+}
